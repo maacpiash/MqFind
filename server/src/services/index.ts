0b7bbe1abbe700eda7afd34e234556ed34da5f5a
Ahad Chowdhury <ahadc@maacpiash.com>
2020-01-21 16:59:14 +0600

    Implement loading details of all listings at once
    Prior to this change, listings from only the first page of the search
    results were being loaded.
    
    This change implements the loading logic in such a way that all the
    results from all the pages are loaded at once.
    
    However, the solution is not optimum. The first page has to load twice
    in order to get the total number of listings.

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,11 +1,13 @@
 import GetListingLinks from './listings'
 import FilterListings from './parser'
 import { IQuery } from '../models/interfaces'
+import { Accommodation } from '../models/accommodation'
+import { OptionsPerPage } from '../models/constants'
 
-export default function getListings(
+export function getListings(
   query: IQuery,
   pageNum?: number,
-): Promise<any> {
+): Promise<(Accommodation | number)[]> {
   return GetListingLinks(
     query.housingOption,
     query.campusName,
@@ -16,5 +18,31 @@ export default function getListings(
       new Promise(res => res(resp.numberOfOptions)),
       ...resp.links.map(FilterListings),
     ]),
+  )
+}
+
+export default function getAllListings(
+  query: IQuery,
+): Promise<(number | Accommodation)[]> {
+  return getListings(query).then(array => {
+    let promise = getListings(query)
+    const max = (array[0] as number) / OptionsPerPage + 1
+    for (let i = 2; i < max; i++) {
+      promise = promise.then(arr => list(query, arr, i))
+    }
+    return promise
+  })
+}
+
+function list(
+  query: IQuery,
+  arr: (number | Accommodation)[],
+  page: number,
+): Promise<Accommodation[]> {
+  return getListings(query, page).then(
+    newarr =>
+      arr
+        .concat(newarr)
+        .filter(item => typeof item !== 'number') as Accommodation[],
   )
 }
