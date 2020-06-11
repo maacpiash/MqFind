9b6384b1a1b0564a4786b2cc919fc020bf28bf93
Ahad Chowdhury <ahad@maacpiash.com>
2020-06-11 22:18:18 +1000

    Add license: `AGPL-3.0-only`

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,3 +1,19 @@
+/*
+ * MqFind: Query listings of accommodation near Macquarie University campuses
+ * Copyright (C) 2020  Mohammad Abdul Ahad Chowdhury
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3,
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import * as React from 'react'
 import Form from './Form'
 import { AppState, FormState } from '../types'
