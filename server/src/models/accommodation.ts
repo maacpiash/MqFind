c888c76e96491a330749c5f84eb8196d3e2ae05b
Ahad Chowdhury <ahad@maacpiash.com>
2020-07-02 01:40:23 +1000

    Fix incorrect distance issue

--- server/src/models/accommodation.ts
+++ server/src/models/accommodation.ts
@@ -1,16 +1,16 @@
 /*
  * MqFind: Query listings of accommodation near Macquarie University campuses
  * Copyright (C) 2020  Mohammad Abdul Ahad Chowdhury
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License version 3,
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -154,7 +154,7 @@ export class Accommodation {
       dict['Common Areas Accessible']?.[0].split(', ') ?? []
     this.rentMin = Number(stats[0][1].split('$')[1]) || 0
 
-    const NorthRydeFirst = stats[1].includes('North Ryde')
+    const NorthRydeFirst = stats[1].includes('North')
     const CityFirst = stats[1].includes('City')
     const first = Number(stats[1][0])
     const second = Number(stats[2][0])
