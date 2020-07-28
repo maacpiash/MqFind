087cbedfb90e68551a7a0177c070b3ce990f2017
Ahad Chowdhury <ahad@maacpiash.com>
2020-07-29 03:53:06 +1000

    Convert function component to class component
    Also,
    - Add license header
    - Fix style (width)

--- packages/client/src/components/form/SelectGenders.tsx
+++ packages/client/src/components/form/SelectGenders.tsx
@@ -1,3 +1,19 @@
+/*
+ * MqFind: Query listings of accommodation near Macquarie University campuses
+ * Copyright (C) 2020  Mohammad Abdul Ahad Chowdhury
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import * as React from 'react'
 import { Dropdown, IDropdownOption } from '@fluentui/react/lib'
 
@@ -6,33 +22,42 @@ const genders = [
   { key: 'M', text: 'Male' },
 ]
 
-const SelectGenders: React.FunctionComponent = () => {
-  const [selectedKeys, setSelectedKeys] = React.useState<string[]>([])
+type SelectProps = { setSelectedGenders: (values: string[]) => any }
+type SelectState = { selectedGenders: string[] }
 
-  const onChange = (
+class SelectGenders extends React.Component<SelectProps, SelectState> {
+  constructor(props: any) {
+    super(props)
+    this.state = { selectedGenders: [] }
+  }
+
+  onChange(
     _event: React.FormEvent<HTMLDivElement>,
-    item: IDropdownOption | undefined,
-  ): void => {
-    if (item) {
-      setSelectedKeys(
-        item.selected
-          ? [...selectedKeys, item.key as string]
-          : selectedKeys.filter((key) => key !== item.key),
-      )
+    dropdown?: IDropdownOption,
+  ) {
+    if (dropdown) {
+      const selectedGenders = dropdown.selected
+        ? [...this.state.selectedGenders, dropdown.key as string]
+        : this.state.selectedGenders.filter((key) => key !== dropdown.key)
+      this.setState({ selectedGenders })
+      this.props.setSelectedGenders(selectedGenders)
     }
   }
 
-  return (
-    <Dropdown
-      placeholder="Multiple choice"
-      label="Prefered Gender(s)"
-      selectedKeys={selectedKeys}
-      onChange={onChange}
-      multiSelect
-      options={genders}
-      style={{ width: '300px' }}
-    />
-  )
+  render() {
+    const { selectedGenders } = this.state
+    return (
+      <Dropdown
+        placeholder="Multiple choice"
+        label="Prefered Gender(s)"
+        selectedKeys={selectedGenders}
+        onChange={this.onChange.bind(this)}
+        multiSelect
+        options={genders}
+        style={{ maxWidth: '300px' }}
+      />
+    )
+  }
 }
 
 export default SelectGenders
